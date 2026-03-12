8ec18f6547126c54a24bd45b696fe3ad066ebfe4
Naseeb Khan <110621136+Naseebullah-Khan@users.noreply.github.com>
2026-03-12 12:54:18 +0430

    Creating a Question Class & Classes and Objects

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler_flutter/question.dart';
 
 void main() => runApp(MyApp());
 
@@ -30,12 +31,19 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
+
+  List<Question> questions = [
+    Question(
+      questionText: 'You can lead a cow down stairs but not up stairs.',
+      questionAnswer: false,
+    ),
+    Question(
+      questionText: 'Approximately one quarter of human bones are in the feet.',
+      questionAnswer: true,
+    ),
+    Question(questionText: 'A slug\'s blood is green.', questionAnswer: true),
   ];
-  List<bool> answers = [false, true, true];
+
   int questionIndex = 0;
 
   @override
@@ -50,7 +58,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionIndex],
+                questions[questionIndex].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 25.0, color: Colors.white),
               ),
@@ -68,7 +76,7 @@ class _QuizPageState extends State<QuizPage> {
               child: Text('True', style: TextStyle(fontSize: 20.0)),
               onPressed: () {
                 setState(() {
-                  if (answers[questionIndex] == true) {
+                  if (questions[questionIndex].questionAnswer == true) {
                     questionIndex++;
                     scoreKeeper.add(Icon(Icons.check, color: Colors.green));
                   } else {
@@ -91,7 +99,7 @@ class _QuizPageState extends State<QuizPage> {
               child: Text('False', style: TextStyle(fontSize: 20.0)),
               onPressed: () {
                 setState(() {
-                  if (answers[questionIndex] == false) {
+                  if (questions[questionIndex].questionAnswer == false) {
                     questionIndex++;
                     scoreKeeper.add(Icon(Icons.check, color: Colors.green));
                   } else {
